329d1850f618d06fa19026f0a936dab91ba71b49
A3h1nt <56585189+A3h1nt@users.noreply.github.com>
2025-04-08 08:37:37 +0530

    Update dnsdumpster.rb
    Some error handling

--- modules/dnsdumpster.rb
+++ modules/dnsdumpster.rb
@@ -2,17 +2,21 @@ module Dnsd
 	$records = ["a","cname","mx","ns"]
 	$dnsd_host = []
 	def Dnsd.getsub(domain)
-		resp = HTTP.headers("X-API-Key" => "#{$dnsdumpster_api_key}").get "https://api.dnsdumpster.com/domain/#{domain}"
-		respj = JSON.parse(resp.body)
-		$records.each do |k|
-			respj[k].each do |h|
-#				puts h['host'].chomp
-				$dnsd_host << h['host'].chomp
+		begin
+			resp = HTTP.headers("X-API-Key" => "#{$dnsdumpster_api_key}").get "https://api.dnsdumpster.com/domain/#{domain}"
+			respj = JSON.parse(resp.body)
+			$records.each do |k|
+				respj[k].each do |h|
+#					puts h['host'].chomp
+					$dnsd_host << h['host'].chomp
+				end
 			end
+		rescue => e
+			print "Some Error Occured : #{e}"
 		end	
 		Fileop.write('dnsd.txt',$dnsd_host)
 		$temp_list = ($temp_list+$dnsd_host).uniq
 		Fileop.append('temp.txt',$temp_list)
-		return $temp_list
+		return $temp_list	
 	end	
 end
