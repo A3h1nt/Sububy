ecf52fbb0405dcf1ecafb530e56a0ceda89de3cb
A3h1nt <56585189+A3h1nt@users.noreply.github.com>
2025-04-08 08:37:10 +0530

    Update cert.rb
    Some error handling

--- modules/cert.rb
+++ modules/cert.rb
@@ -4,15 +4,19 @@ module Cert
 		cl = HTTP.headers(
 		                "User-Agent"=>"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
 		        )
-		resp = cl.get "https://crt.sh/json?q=#{domain}"
-		respj = JSON.parse(resp.body)
-		respj.each do |subds| 
-#			puts subds['common_name']
-			cert_host << subds['common_name']
+		begin        
+			resp = cl.get "https://crt.sh/json?q=#{domain}"
+			respj = JSON.parse(resp.body)
+			respj.each do |subds| 
+#				puts subds['common_name']  
+				cert_host << subds['common_name']
+			end
+		rescue => e
+			puts "Some error occured : #{e}"
 		end
-		Fileop.write('cert.txt',cert_host)
-		$temp_list = (cert_host+$temp_list).uniq
-		Fileop.append('temp.txt',$temp_list)
-		return $temp_list
+			Fileop.write('cert.txt',cert_host)
+			$temp_list = (cert_host+$temp_list).uniq
+			Fileop.append('temp.txt',$temp_list)
+			return $temp_list
 	end
 end
