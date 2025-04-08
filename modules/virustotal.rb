6e687dcf55a9e981ff6540b2f2dd83db7f556853
A3h1nt <56585189+A3h1nt@users.noreply.github.com>
2025-04-08 08:38:09 +0530

    Update virustotal.rb
    some err handling

--- modules/virustotal.rb
+++ modules/virustotal.rb
@@ -1,9 +1,13 @@
 module Vtotal
 	def Vtotal.getsub(domain)
 		vt_host = []
-		resp = HTTP.headers("x-apikey"=>"#{$virustotal_api_key}").get "https://www.virustotal.com/api/v3/domains/#{domain}/subdomains?limit=40"
-		respj = JSON.parse(resp.body)
-		respj['data'].each {|d|vt_host<<d['id']}
+		begin
+			resp = HTTP.headers("x-apikey"=>"#{$virustotal_api_key}").get "https://www.virustotal.com/api/v3/domains/#{domain}/subdomains?limit=40"
+			respj = JSON.parse(resp.body)
+			respj['data'].each {|d|vt_host<<d['id']}
+		rescue => e
+			puts "Some error occured : #{e}"
+		end
 		Fileop.write('vtotal.txt',vt_host)
 		$temp_list = ($temp_list+vt_host).uniq
 		Fileop.append('temp.txt',$temp_list)
