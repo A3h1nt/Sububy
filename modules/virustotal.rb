1696341b5ddcbd8634e1693d0dd9ae565d5d2720
A3h1nt <56585189+A3h1nt@users.noreply.github.com>
2025-04-09 18:41:01 +0530

    Update virustotal.rb

--- modules/virustotal.rb
+++ modules/virustotal.rb
@@ -8,6 +8,7 @@ module Vtotal
 		rescue => e
 			puts "Some error occured : #{e}"
 		end
+		vt_host = vt_host.uniq
 		Fileop.write('vtotal.txt',vt_host)
 		$temp_list = ($temp_list+vt_host).uniq
 		Fileop.append('temp.txt',$temp_list)
