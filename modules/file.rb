44e02d79c7b4bb6ce8bd147ce977a4ca6e87c967
A3h1nt <56585189+A3h1nt@users.noreply.github.com>
2025-04-09 18:40:26 +0530

    Update file.rb

--- modules/file.rb
+++ modules/file.rb
@@ -2,8 +2,29 @@ module Fileop
 
 	def Fileop.write(filename,data_array)
 		fh = File.new "#{$output_dir}/#{filename}","w"
-		data_array.each {|d| fh.write(d+"\n")}
+		data_array.each {|d| fh.write("#{d}\n")}
 		fh.close
+	end
+
+	def Fileop.writeS(filename,data_string)
+		fh = File.new "#{$output_dir}/#{filename}","w"
+		fh.write(data_string)
+		fh.close
+	end
+
+	def Fileop.readS(filename)
+		fh = File.new "#{$output_dir}/#{filename}","r"
+		data = fh.readlines
+		fh.close
+		return data
+	end
+
+	def Fileop.readw(filename)
+		data = []
+		fh = File.new "#{filename}","r"
+		fh.readlines.each { |d| data << d.chomp }
+		fh.close
+		return data
 	end
 
 	def Fileop.read(filename)
@@ -16,16 +37,8 @@ module Fileop
 
 	def Fileop.append(filename,data_array)
 		fh = File.new "#{$output_dir}/#{filename}","a"
-		data_array.each {|d| fh.write(d+"\n")}
+		data_array.each {|d| fh.write("#{d}\n")}
 		fh.close
-	end
-
-	def Fileop.readw(filename)
-		data = []
-		fh = File.new "#{filename}","r"
-		fh.readlines.each { |d| data << d.chomp }
-		fh.close
-		return data
 	end
 
 	def Fileop.fcheck(filename)
